48d2a4c3324dbd210f79c3a92a5e911ccddd9074
iammburg <ilhamprabowo009@gmail.com>
2024-12-07 14:24:41 +0700

    feat: update testimonial name to Saul Pria Baik

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -396,7 +396,7 @@
                                 <div class="text pt-4">
                                     <p class="mb-4">Far far away, behind the word mountains, far from the countries
                                         Vokalia and Consonantia, there live the blind texts.</p>
-                                    <p class="name">Roger Scott</p>
+                                    <p class="name">Saul Pria Baik</p>
                                     <span class="position">Marketing Manager</span>
                                 </div>
                             </div>
