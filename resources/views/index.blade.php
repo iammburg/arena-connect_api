8c217bbd96e09c9943b4976396da63e5053e95f2
iammburg <ilhamprabowo009@gmail.com>
2024-12-07 09:29:51 +0700

    feat: add deployment workflow for Arena Connect and update testimonial heading

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -382,7 +382,7 @@
             <div class="row justify-content-center mb-5">
                 <div class="col-md-7 text-center heading-section ftco-animate">
                     <span class="subheading">Testimoni</span>
-                    <h2 class="mb-3">Berbagai Testimoni dari Customer</h2>
+                    <h2 class="mb-3">Berbagai Testimoni dari Pelanggan</h2>
                 </div>
             </div>
             <div class="row ftco-animate">
